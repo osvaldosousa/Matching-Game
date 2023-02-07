313d735673a2e4b4609861128cf38d863c08c898
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-02-07 15:54:46 -0300

    create flipper function

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,22 @@
-import { CardGame } from '../CardGame'
+import './style.css';
+import { CardGame } from '../CardGame';
 
 export function CardFrontBack() {
+  window.cardFlip = {};
+  window.cardFlip.handleClick = (event) => {
+    const $origin = event.target;
+    const coiso = $origin.closest('.card-flip');
+    coiso.classList.toggle('-active');
+  }
+
   return /* html */ `
-    <article class="card-flip">
-      ${CardGame()}
-      ${CardGame('javascript', 'LOgo')}
+    <article class="card-flip" onClick="cardFlip.handleClick(event)">
+      <div class="card-front">
+        ${CardGame()}
+      </div>
+      <div class="card-back">
+        ${CardGame('javascript', 'LOgo')}
+      </div>
     </article>
   `
 }
