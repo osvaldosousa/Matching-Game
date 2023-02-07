9ac9c402be13978cb87ac7136b0e99c5a39bf72f
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-02-07 17:28:07 -0300

    resolve chanllenge toptoptop
    minha solução para o flipe das cartas ja viradas

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,11 +2,31 @@ import './style.css';
 import { CardGame } from '../CardGame';
 
 export function CardFrontBack() {
+  let coiso = 0;
   window.cardFlip = {};
+
+  window.cardFlip.hiddenCard = (event) => {
+    setTimeout(() => {
+      const cardsHidden = event.target.closest('.board-game');
+      let filhos = cardsHidden.querySelectorAll('.-active');
+      filhos.forEach(element => {
+        element.classList.toggle('-active');
+      });
+    }, 2000);
+  }
+
   window.cardFlip.handleClick = (event) => {
     const $origin = event.target;
-    const coiso = $origin.closest('.card-flip');
-    coiso.classList.toggle('-active');
+    const cardToggle = $origin.closest('.card-flip');
+    coiso++;
+
+    if(coiso >= 2) {
+      const cards = event
+      cardToggle.classList.toggle('-active');
+      cardFlip.hiddenCard(cards);
+      return
+    }
+    cardToggle.classList.toggle('-active');
   }
 
   return /* html */ `
