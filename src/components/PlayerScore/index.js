c7b5ef788d2bef42c4378daab3374b4505962f5a
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-02-09 15:43:36 -0300

    new feature in PlayScore
    add feature swapPlayer

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -1,6 +1,18 @@
 import './style.css';
 
 export function PlayerScore(score = 0) {
+  const swapPlayer = () => {
+    const $arrowDown = document.querySelector('.arrow-down');
+    const currentPlayer = $arrowDown.getAttribute('data-currentplayer');
+    $arrowDown.setAttribute('data-currentplayer', currentPlayer == 1 ? 2 : 1)
+  };
+
+  window.playerScore = {};
+
+  window.playerScore.scoreGame = () => {
+    swapPlayer()
+  };
+
   return /* html */ `
     <div 
       class="bar-score"
